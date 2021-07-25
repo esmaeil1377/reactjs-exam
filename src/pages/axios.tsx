31b4e754dabaffdbcb88ff9cbb7cc295a71c55a2
esmaeil1377 <pahang1378@gmail.com>
2021-07-25 17:49:38 +0430

    second change

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import IUser from "../types/user";
+import {data} from "browserslist";
 // you don't need anything else to import
 
 interface Props {
@@ -10,17 +11,24 @@ interface Props {
 export default function AxiosTest(props: Props) {
   // TODO: once you get data map through data and show them name
   // return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
-  const promise = axios.get("/api/users");
-  const dataPromise = promise.then((response) => response.data)
   const { username } = props;
+  axios.get("/api/users").then(response => {
+  console.log(response.data);
+  for (let i = 0; i < response.data.length; i++) {
+    console.log(response.data[i]);
+  }
+  });
   return <div>{username}</div>
 }
 
-axios.get("/api/users").then(response => {
-  console.log(response.data[name]);
-  // AxiosTest(response.data[0]);
-
-});
+// axios.get("/api/users").then(response => {
+//   console.log(response.data);
+//   for (let i = 0; i < response.data.length; i++) {
+//     console.log(response.data[i]);
+//   }
+//   // AxiosTest(response.data[0]);
+//
+// });
 // TODO: fetch data with axios to use in app
 // RestApi: axios.get("/api/users") => [{name: "amir"}]
 // NOTE: data fetching should happen in server side
