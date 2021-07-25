298622d38417c3f5e6014f287f5486269fb9fa83
esmaeil1377 <pahang1378@gmail.com>
2021-07-25 17:30:26 +0430

    first change

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -4,13 +4,23 @@ import IUser from "../types/user";
 
 interface Props {
   // TODO: add user type
+  username: string;
 }
 
 export default function AxiosTest(props: Props) {
   // TODO: once you get data map through data and show them name
-  return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
+  // return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
+  const promise = axios.get("/api/users");
+  const dataPromise = promise.then((response) => response.data)
+  const { username } = props;
+  return <div>{username}</div>
 }
 
+axios.get("/api/users").then(response => {
+  console.log(response.data[name]);
+  // AxiosTest(response.data[0]);
+
+});
 // TODO: fetch data with axios to use in app
 // RestApi: axios.get("/api/users") => [{name: "amir"}]
 // NOTE: data fetching should happen in server side
--- src/pages/formik.tsx
+++ src/pages/formik.tsx
@@ -1,4 +1,5 @@
 import { Input, Form, Button, Space } from "antd";
+import { useFormik } from 'formik';
 
 const FormItem = Form.Item;
 
@@ -12,35 +13,73 @@ export default function FormikTest() {
   // BONUS: adding validation has extra points
   // Resource: https://formik.org/docs/tutorial
 
+    const formik = useFormik({
+     initialValues: {
+       username: '',
+       password: '',
+     },
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+        onReset: values => {
+         // values.username = "";
+         // values.password = "";
+            formik.values.password = '';
+            formik.values.username = '';
+        }
+   });
+
   const handleSubmit = (values: IForm) => {
     console.log(values);
   };
+
+  const handleReset = (values: IForm) => {
+      console.log(values);
+
+  }
 
   return (
     <div>
       <div>Simple Formik With Antd Inputs :: Edit src/pages/formik.tsx</div>
       <hr />
       {/* TODO: use Formik */}
-      <form>
+      <form onSubmit={formik.handleSubmit} onReset={formik.handleReset}>
         {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
         <FormItem label="Username" name="username">
-          <Input />
+          <input
+         id="username"
+         name="username"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.username}
+       />
         </FormItem>
         {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
         <FormItem label="Password" name="password">
-          <Input />
+          <input
+         id="password"
+         name="password"
+         type="password"
+         onChange={formik.handleChange}
+         value={formik.values.password}
+       />
         </FormItem>
         <Space>
-          <Button type="primary" htmlType="submit">
-            submit
-          </Button>
-          <Button
-            onClick={() => {
-              // TODO: reset form with formik
-            }}
-          >
-            reset
-          </Button>
+          {/*<Button type="primary" htmlType="submit">*/}
+          {/*  submit*/}
+          {/*</Button>*/}
+          <button type="submit">submit</button>
+          {/*<button type="reset" onClick={*/}
+          {/*  */}
+          {/*}>reset</button>*/}
+          <button onClick={formik.handleReset} type="reset">
+              reset
+          </button>
+          {/*    // TODO: reset form with formik*/}
+          {/*  }}*/}
+          {/*>*/}
+          {/*  reset*/}
+          {/*</Button>*/}
         </Space>
       </form>
     </div>
